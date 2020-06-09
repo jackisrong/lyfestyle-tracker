822f89228c6ce2bb6163f604e2f1d547d0f71e40
Luis Nogales <luis.e.nogales@gmail.com>
2020-06-08 19:43:29 -0700

    added the sleep activity and a datePicker activity

---
+++ app/src/main/java/com/example/lyfestyletracker/DatePicker.java
@@ -0,0 +1,14 @@
+package com.example.lyfestyletracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatePicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+    }
+}
