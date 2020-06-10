78521812d252ec4fff82329b1792527250352267
jackisrong <34490671+jackisrong@users.noreply.github.com>
2020-06-09 22:07:48 -0700

    Moved MainActivity functionality into UserLogin

--- app/src/main/java/com/example/lyfestyletracker/UserLogin.java
+++ app/src/main/java/com/example/lyfestyletracker/UserLogin.java
@@ -2,6 +2,7 @@ package com.example.lyfestyletracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class UserLogin extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class UserLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        Intent intent = new Intent(this, SleepTracker.class);
+        startActivity(intent);
     }
 }
