e21b2ae2546f8bb2822a7946159621a3f0545a87
jackisrong <34490671+jackisrong@users.noreply.github.com>
2020-06-10 12:52:35 -0700

    Renamed UserLogin to Login

--- app/src/main/java/com/example/lyfestyletracker/Login.java
+++ app/src/main/java/com/example/lyfestyletracker/Login.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class UserLogin extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_login);
+        setContentView(R.layout.activity_login);
     }
 
     public void authenticate(View view) {
