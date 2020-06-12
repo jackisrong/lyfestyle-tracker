5bdb5ba241f9ec8732fa2e28c2816f664da18711
Luis Nogales <luis.e.nogales@gmail.com>
2020-06-11 22:11:59 -0700

    added an activity to add sleep entries

---
+++ app/src/main/java/com/example/lyfestyletracker/addSleepEntry.java
@@ -0,0 +1,14 @@
+package com.example.lyfestyletracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addSleepEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sleep_entry);
+    }
+}
