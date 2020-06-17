9f4326623cd3bdaad4d552ba5bdb364679b2aa66
Luis Nogales <luis.e.nogales@gmail.com>
2020-06-16 23:59:46 -0700

    added comments to the queries

--- sql_code/queries.sql
+++ sql_code/queries.sql
@@ -7,41 +7,45 @@
 //selects the user sleep times for the past week
 "Select us.username, us.sleepTime from UserSleepEntry us WHERE us.username = " + username + " AND sleepDate >= TRUNC(SYSDATE, 'DY') AND us.sleepDate < TRUNC(SYSDATE, 'DY') + 7 ORDER BY us.sleepdate"
 
+
+//Inserting into sleep entries acording to two spinner inputs
 "INSERT INTO userSleepEntry VALUES ('"+ username + "', TO_DATE('" + dateResult.getText() +"', 'YYYY-MM-DD'), " + s1.getSelectedItem().toString() + ", " + s2.getSelectedItem().toString() +")"
 
-
+//Updates the workout according to values from the user
 "UPDATE Workout SET description = '" + workoutDesc.getText().toString() + "', caloriesburnt = " + workoutCaloriesBurnt.getText().toString() + ", timeworkout = " + workoutLength.getText().toString() + " WHERE workoutid = " + workoutId"
 
-"DELETE FROM UserExerciseLog WHERE workoutid = " + workoutId "AND logtime = TO_TIMESTAMP('" + dateResult + " " + timeResult + "', 'YYYY-MM-DD HH24:MI:SS') AND username = '" + username +
+//selects the userPeople who match the given username
+"SELECT * FROM People p, UserPerson up WHERE up.username = ' + username + ' AND p.username = up.username"
 
-'SELECT * FROM People p, UserPerson up WHERE up.username = '' + username + '' AND p.username = up.username'
+//selects the consultants who match the given username
 'SELECT * FROM People p, Consultant c WHERE c.username = ' + username  + ' AND c.username = p.username'
 
-"SELECT name FROM people WHERE username = '" + username + "'""
 
-
+//selecting the the exercise log entries that match a username and are sorted in descending order of log time
 "SELECT ele.logtime, w.description, w.caloriesburnt, w.timeworkout FROM workout w, exerciselogentry ele, userexerciselog uel WHERE w.workoutid = ele.workoutid AND w.workoutid = uel.workoutid AND ele.logtime = uel.logtime AND uel.username = '"+ username + "' ORDER BY uel.logtime DESC"
 
-"SELECT uml.logTime, m.description, mle.numberOfServings, m.type FROM userMealLog uml, Meal m, MealLogEntry mle WHERE uml.username = 'bob123' AND mle.mealId = m.mealID AND uml.mealId = mle.mealID AND uml.logTime = mle.logTime ORDER BY mle.logTime DESC"
 
 
-
+//Selects the combination of consultant usernames and companies they work for ordered by their rating
 "Select c.username, c.result, co.name FROM Consultant c, Company co, ConsultantWorksForCompany cw WHERE cw.consultantUsername = c.username AND co.companyID = cw.companyId AND c.username IN (SELECT consultantUsername FROM UserHiresConsultant WHERE userUsername = '" + mParam1 +"' AND c.username = consultantUsername) ORDER BY c.result"
 
+//Selects the username and rating from the consultants who arent currently  hired by the user
+"Select c.username, c.result FROM Consultant c WHERE c.username NOT IN (SELECT consultantUsername FROM UserHiresConsultant WHERE userUsername = '" + username + "') ORDER BY c.result"
 
-"Select c.username, c.result, co.name FROM Consultant c, Company co, ConsultantWorksForCompany cw WHERE cw.consultantUsername = c.username AND co.companyID = cw.companyId AND c.username NOT IN (SELECT consultantUsername FROM UserHiresConsultant WHERE userUsername = 'bob123') ORDER BY c.result"
-
-
+//Selects the users who have hired the current consultant shown by mParam1
 "Select u.username, p.email, uhc.contractNumber From UserPerson u, UserHiresConsultant uhc, People p Where uhc.userUsername = u.username AND p.username = u.username AND uhc.consultantUsername = '" +mParam1+ "'"
 
+//division statement that show all hired users who have had all of the plans created by the current consultant suggested to them
 "Select u.username, p.email, uhc.contractNumber From UserPerson u, UserHiresConsultant uhc, People p Where uhc.userUsername = u.username AND p.username = u.username AND uhc.consultantUsername = '" +mParam1+ "' AND NOT EXISTS ((Select planId FROM Plan WHERE createdByUsername = uhc.consultantUsername) MINUS (Select csp.planId FROM ConsultantSuggestsPlan csp Where csp.userUsername = u.username AND csp.consultantUsername = uhc.consultantUsername))"
 
+//consultant suggests a plan to a user
 "Insert Into ConsultantSuggestsPlan Values('" + userUsername + "', '" + username + "', " + plan + ", TO_TIMESTAMP('" + formattedDate + "',  'YYYY-MM-DD HH24:MI:SS'))"
 
 
-
+//selecting the user details from a userPerson
 "Select age, weight, height from UserPerson where username = '" + username + "'"
 
+//updating the values of the user
 "UPDATE userPerson SET " + column + " = " + setTo + " where username = '" + username + "'"
 
 
@@ -57,7 +61,7 @@
                          "', TO_TIMESTAMP('" + dateResult + " " + timeResult + "', 'YYYY-MM-DD HH24:MI:SS'), " + mealId.getText().toString() + ")"
 
 
- //Inserting a Workou//ADDING A WORKOUT
+ //Inserting a Workoukout ADDING A WORKOUT
  "Insert Into Workout Values (" + workoutId.getText().toString() + ", '"
                      + workoutDesc.getText().toString() + "', " + workoutCaloriesBurnt.getText().toString() + ", "
                      + workoutLength.getText().toString() + ")"
@@ -73,18 +77,20 @@
 
 
 
+//user hires a consultant
+ "Insert Into UserHiresConsultant Values('" + username + "', '" + usernameC + "', " + rand + ")"
 
- "Insert Into UserHiresConsultant Values('" + username + "', '" + usernameC +"', TO_TIMESTAMP('" + timeStamp +"', 'YYYY-MM-DD HH24:MI:SS'))"
+//Select all the workout plan details created by David
+"SELECT planId, createdByUsername, exercisePerWeek FROM Plan, WorkoutPlan WHERE createdByUsername = 'David' AND planId = workoutPlanId"
 
-
-
- "SELECT planId, createdByUsername, exercisePerWeek FROM Plan, WorkoutPlan WHERE createdByUsername = 'David' AND planId = workoutPlanId"
-
+//Select all the diet details created by David
 "SELECT planId, createdByUsername, weeklyCalories FROM Plan, Diet WHERE createdByUsername = '" + username + "' AND planId = dietId"
 
+//get all the plans suggested to a user
 "SELECT planID, consultantUsername as createdByUsername, logTime FROM ConsultantSuggestsPlan WHERE userUsername = '" + username + "' "
 
-
+//get all of the meal log entries from a diet that match a search description and are ordered by one of the columns of the table
 "Select m.mealId, ml.logTime, m.description, m.servingSizeGrams, m.type From DietContainsMealLog dcm, Meal m, MealLogEntry ml WHERE dcm.dietID = " + dietPlanID +" AND ml.logTime = dcm.logTime AND ml.mealId = dcm.mealId AND LOWER(m.description) LIKE '%" + searchTerm.toLowerCase() + "%' ORDER BY " + sortBy + " " + sortByOrder
 
+//get all of the meal log entries from a diet that match a search description and are ordered by one of the columns of the table with different columns
 "SELECT m.mealid, uml.logTime, m.description, mle.numberOfServings, m.type FROM userMealLog uml, Meal m, MealLogEntry mle WHERE uml.username = '" + username + "' AND mle.mealId = m.mealID AND uml.mealId = mle.mealID AND uml.logTime = mle.logTime AND LOWER(m.description) LIKE '%" + searchTerm.toLowerCase() + "%' ORDER BY " + sortBy + " " + sortByOrder
